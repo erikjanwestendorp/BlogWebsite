e3cf656c2256bb3140b40b7a8dff86a3cca31ccb
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-09-12 14:46:48 +0200

    Create HeaderBase component

---
+++ BlogWebsite.Frontend/src/components/header/HeaderBase.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header">
+      <h1 v-if="title">{{title}}</h1>
+      <img v-if="url" :src="url" :alt="alt">
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+
+  props:{
+      title: String,
+      url: String,
+      alt: String,
+  },
+  setup() {
+  }
+});
+</script>
+
+<style lang="scss">
+.default {
+}
+</style>
