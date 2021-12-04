c0ad3f48a5893e85381a364c4e84a114f953efb2
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-12-04 10:59:25 +0100

    Create images block

---
+++ BlogWebsite.Frontend/src/components/blocks/ImagesBlock.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="images-block">
+      <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+
+});
+</script>
+<style lang="scss">
+.images-block {
+}
+</style>
