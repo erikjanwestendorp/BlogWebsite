1966608e9b8387fc9ce78d6123cdefeaeef9b2aa
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2021-12-04 11:33:51 +0100

    Videoblock

---
+++ BlogWebsite.Frontend/src/components/blocks/VideoBlock.vue
@@ -0,0 +1,24 @@
+<template>
+  <Block class="video-block">
+      <iframe v-if="external" :src="url"></iframe>
+      <video v-if="!external" :src="url"></video>
+      <img v-if="placeholder" :src="placeholder" :alt="placeholderAlt">
+  </Block>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Block from "./_Block.vue";
+export default defineComponent({
+  props:{
+      external: Boolean,
+      url: String,
+      placeholder: String,
+      placeholderAlt : String
+  },
+  components: {Block}
+});
+</script>
+<style lang="scss">
+.video-block {
+}
+</style>
