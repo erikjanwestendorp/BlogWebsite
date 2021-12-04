d0e8901f2559f89d5cbff51281bc32ad17978dd3
Mike Mijnheer <mike.mijnheer@hotmail.com>
2021-12-04 12:23:16 +0100

    Created templates // ModelsBuilder // Small fixes

--- BlogWebsite.Data/Models/ModelsBuilder/SliderComponent.generated.cs
+++ BlogWebsite.Data/Models/ModelsBuilder/SliderComponent.generated.cs
@@ -19,13 +19,13 @@ using Umbraco.Extensions;
 namespace BlogWebsite.Data.Models.ModelsBuilder
 {
 	/// <summary>Slider</summary>
-	[PublishedModel("slider")]
-	public partial class Slider : PublishedElementModel, IComponentComposition
+	[PublishedModel("sliderComponent")]
+	public partial class SliderComponent : PublishedElementModel, IComponentComposition
 	{
 		// helpers
 #pragma warning disable 0109 // new is redundant
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.1+b58a0cf0a5b3b695f09b8884a704d2264b32e253")]
-		public new const string ModelTypeAlias = "slider";
+		public new const string ModelTypeAlias = "sliderComponent";
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.1+b58a0cf0a5b3b695f09b8884a704d2264b32e253")]
 		public new const PublishedItemType ModelItemType = PublishedItemType.Content;
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.1+b58a0cf0a5b3b695f09b8884a704d2264b32e253")]
@@ -34,14 +34,14 @@ namespace BlogWebsite.Data.Models.ModelsBuilder
 			=> PublishedModelUtility.GetModelContentType(publishedSnapshotAccessor, ModelItemType, ModelTypeAlias);
 		[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Umbraco.ModelsBuilder.Embedded", "9.1.1+b58a0cf0a5b3b695f09b8884a704d2264b32e253")]
 		[return: global::System.Diagnostics.CodeAnalysis.MaybeNull]
-		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<Slider, TValue>> selector)
+		public static IPublishedPropertyType GetModelPropertyType<TValue>(IPublishedSnapshotAccessor publishedSnapshotAccessor, Expression<Func<SliderComponent, TValue>> selector)
 			=> PublishedModelUtility.GetModelPropertyType(GetModelContentType(publishedSnapshotAccessor), selector);
 #pragma warning restore 0109
 
 		private IPublishedValueFallback _publishedValueFallback;
 
 		// ctor
-		public Slider(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
+		public SliderComponent(IPublishedElement content, IPublishedValueFallback publishedValueFallback)
 			: base(content, publishedValueFallback)
 		{
 			_publishedValueFallback = publishedValueFallback;
